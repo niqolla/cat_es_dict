37f20f12e5be1d38492c06bc7475a254dae6274d
niqolla <n.panajotovic@outook.com>
2024-11-24 18:23:09 +0100

    to update vercel

--- dict/views.py
+++ dict/views.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 df = pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT79R5fgLfFdx1_Y9EAI7Kk3ysJ-QfNtsjiu7H1uqwUQ5afvi4L3nj3C6x-M00rWcDJvUYQZx2Sh4uB/pub?gid=0&single=true&output=csv')
 
+
 def index(request):
     vars = df.query('important == "*"').sample(100).to_numpy().tolist()
     return render(request, 'dict/index.html', {'vars':vars})
